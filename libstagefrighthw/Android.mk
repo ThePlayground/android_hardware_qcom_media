e998e6760cffa0d823554c51cdce7c35ccce3707
Twisted Playground <twistedumbrella@gmail.com>
2011-12-27 11:04:45 -0500

    qsd8: qcom Updated libstagefrighthw

--- libstagefrighthw/Android.mk
+++ libstagefrighthw/Android.mk
@@ -4,6 +4,10 @@ ifeq ($(TARGET_BOARD_PLATFORM),msm7x30)
     include $(SF_HW_ROOT)/msm7x30/Android.mk
 endif
 
+ifeq ($(TARGET_BOARD_PLATFORM),qsd8k)
+    include $(SF_HW_ROOT)/msm8x60/Android.mk
+endif
+
 ifeq ($(TARGET_BOARD_PLATFORM),msm8660)
     include $(SF_HW_ROOT)/msm8x60/Android.mk
 endif
--- libstagefrighthw/msm8x60/Android.mk
+++ libstagefrighthw/msm8x60/Android.mk
@@ -15,8 +15,8 @@
 #
 
 ifeq ($(TARGET_BOARD_PLATFORM),msm8660)
-LOCAL_PATH := $(call my-dir)
 
+LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := \
@@ -37,5 +37,35 @@ LOCAL_SHARED_LIBRARIES :=       \
 LOCAL_MODULE := libstagefrighthw
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_SHARED_LIBRARY)
+
+endif
+
+ifneq ($(BOARD_USES_QCOM_LEGACY),true)
+
+ifeq ($(TARGET_BOARD_PLATFORM),qsd8)
+
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+    QComOMXPlugin.cpp                      \
+
+LOCAL_CFLAGS := $(PV_CFLAGS_MINUS_VISIBILITY)
+
+LOCAL_C_INCLUDES:= \
+        $(TOP)/frameworks/base/include/media/stagefright/openmax
+
+LOCAL_SHARED_LIBRARIES :=       \
+        libbinder               \
+        libutils                \
+        libcutils               \
+        libdl                   \
+        libui                   \
+
+LOCAL_MODULE := libstagefrighthw
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_SHARED_LIBRARY)
+
+endif
 
 endif
