c024aace3c98a7448072da4b8a9b86c54be5592d
Shalaj Jain <shalajj@codeaurora.org>
2012-01-05 17:58:57 -0800

    libI420colorconvert: Add new library for MovieStudio color conversions

--- Android.mk
+++ Android.mk
@@ -1,7 +1,14 @@
 QCOM_MEDIA_ROOT := $(call my-dir)
 
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
-    ifneq ($(BOARD_USES_AUDIO_LEGACY),true)
+
+ifneq ($(BOARD_USES_AUDIO_LEGACY),true)
 	include $(QCOM_MEDIA_ROOT)/audio/Android.mk
-    endif
 endif
+ifeq ($(BOARD_USES_QCOM_LIBS),true)
+	include $(TOP)/vendor/qcom/opensource/omx/mm-core/Android.mk
+	include $(TOP)/vendor/qcom/opensource/omx/mm-video/Android.mk
+endif
+
+include $(QCOM_MEDIA_ROOT)/libI420colorconvert/Android.mk
+endif # BOARD_USES_QCOM_HARDWARE
