5d9978c6aeb1f14fd553c7525c6e320f8ae1db69
PlaygroundICS <tw1st3dz3ro5@gmail.com>
2012-02-29 02:17:53 -0500

    Unnecessary redundant build call

--- Android.mk
+++ Android.mk
@@ -7,8 +7,4 @@ ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 ifneq ($(BOARD_USES_AUDIO_LEGACY),true)
 	include $(QCOM_MEDIA_ROOT)/audio/Android.mk
 endif
-ifeq ($(BOARD_USES_QCOM_LIBS),true)
-	include $(TOP)/vendor/qcom-opensource/omx/mm-core/Android.mk
-	include $(TOP)/vendor/qcom-opensource/omx/mm-video/Android.mk
-endif
 endif
