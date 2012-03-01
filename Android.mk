13a7c3804f613cf27cd14a3f755de1b8e63944c8
TwistedUmbrella <twistedumbrella@gmail.com>
2012-03-01 12:36:13 -0500

    Refactor QCOM_LEGACY for audio implementation

--- Android.mk
+++ Android.mk
@@ -1,10 +1,14 @@
 QCOM_MEDIA_ROOT := $(call my-dir)
 
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
-	media-hals := libstagefrighthw librpc
-	include $(call all-named-subdir-makefiles,$(media-hals))
 
+ifeq ($(BOARD_USES_LEGACY_QCOM),true)
+	media-hals := librpc
+else
 ifneq ($(BOARD_USES_AUDIO_LEGACY),true)
 	include $(QCOM_MEDIA_ROOT)/audio/Android.mk
 endif
+	media-hals := libstagefrighthw librpc
+endif
+	include $(call all-named-subdir-makefiles,$(media-hals))
 endif
