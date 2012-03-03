e180f9320a5b8dd56413ee176376de5dfb79bbdd
TwistedUmbrella <twistedumbrella@gmail.com>
2012-03-03 03:39:56 -0500

    Restore librpc to msm7k for conditional platform builds

--- Android.mk
+++ Android.mk
@@ -2,13 +2,11 @@ QCOM_MEDIA_ROOT := $(call my-dir)
 
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 
-ifeq ($(BOARD_USES_LEGACY_QCOM),true)
-	media-hals := librpc
-else
 ifneq ($(BOARD_USES_AUDIO_LEGACY),true)
 	include $(QCOM_MEDIA_ROOT)/audio/Android.mk
 endif
-	media-hals := libstagefrighthw librpc
+ifneq ($(BOARD_USES_LEGACY_QCOM),true)
+	include $(QCOM_MEDIA_ROOT)/libstagefrighthw/Android.mk
 endif
-	include $(call all-named-subdir-makefiles,$(media-hals))
+
 endif
