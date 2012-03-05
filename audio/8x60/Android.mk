3dbf6c8f790e4870b0b174e13543f64cdf842531
TwistedUmbrella <twistedumbrella@gmail.com>
2012-03-05 05:05:46 -0500

    Bring msm8660 audio inline w/ current CM9 source

--- audio/8x60/Android.mk
+++ audio/8x60/Android.mk
@@ -1,3 +1,8 @@
+# Copyright 2011 The Android Open Source Project
+
+#AUDIO_POLICY_TEST := true
+#ENABLE_AUDIO_DUMP := true
+
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
@@ -10,7 +15,7 @@ LOCAL_SRC_FILES := \
     audio_hw_hal.cpp
 
 ifeq ($(BOARD_HAVE_BLUETOOTH),true)
-    LOCAL_CFLAGS += -DWITH_A2DP
+  LOCAL_CFLAGS += -DWITH_A2DP
 endif
 
 LOCAL_SHARED_LIBRARIES := \
@@ -22,23 +27,21 @@ LOCAL_SHARED_LIBRARIES := \
     libacdbmapper
 
 ifneq ($(TARGET_SIMULATOR),true)
-    LOCAL_SHARED_LIBRARIES += libdl
+LOCAL_SHARED_LIBRARIES += libdl
 endif
 
 LOCAL_STATIC_LIBRARIES := \
     libmedia_helper \
     libaudiohw_legacy
 
-LOCAL_MODULE := audio.primary.$(TARGET_BOARD_PLATFORM)
+LOCAL_MODULE := audio.primary.$(TARGET_BOOTLOADER_BOARD_NAME)
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_CFLAGS += -fno-short-enums
 
 LOCAL_C_INCLUDES += $(TARGET_OUT_HEADERS)/mm-audio/audio-alsa
-ifeq ($(BOARD_USES_QCOM_AUDIO_CALIBRATION),true)
-    LOCAL_C_INCLUDES += $(TARGET_OUT_HEADERS)/mm-audio/audcal
-endif
+LOCAL_C_INCLUDES += $(TARGET_OUT_HEADERS)/mm-audio/audcal
 LOCAL_C_INCLUDES += $(TARGET_OUT_HEADERS)/mm-audio/audio-acdb-util
 LOCAL_C_INCLUDES += hardware/libhardware/include
 LOCAL_C_INCLUDES += hardware/libhardware_legacy/include
@@ -46,6 +49,7 @@ LOCAL_C_INCLUDES += frameworks/base/include
 LOCAL_C_INCLUDES += system/core/include
 
 include $(BUILD_SHARED_LIBRARY)
+
 
 # The audio policy is implemented on top of legacy policy code
 include $(CLEAR_VARS)
@@ -63,12 +67,12 @@ LOCAL_STATIC_LIBRARIES := \
     libmedia_helper \
     libaudiopolicy_legacy
 
-LOCAL_MODULE := audio_policy.$(TARGET_BOARD_PLATFORM)
+LOCAL_MODULE := audio_policy.$(TARGET_BOOTLOADER_BOARD_NAME)
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_MODULE_TAGS := optional
 
 ifeq ($(BOARD_HAVE_BLUETOOTH),true)
-    LOCAL_CFLAGS += -DWITH_A2DP
+  LOCAL_CFLAGS += -DWITH_A2DP
 endif
 
 LOCAL_C_INCLUDES += hardware/libhardware_legacy/audio
--- audio/Android.mk
+++ audio/Android.mk
@@ -1,17 +1,17 @@
 AUDIO_HW_ROOT := $(call my-dir)
 
 ifeq ($(TARGET_BOARD_PLATFORM),msm8660)
-    include $(AUDIO_HW_ROOT)/msm8x60/Android.mk
+    include $(AUDIO_HW_ROOT)/8x60/Android.mk
 endif
 
 ifeq ($(TARGET_BOARD_PLATFORM),msm7x27a)
-    include $(AUDIO_HW_ROOT)/msm7x27a/Android.mk
+    include $(AUDIO_HW_ROOT)/7x27a/Android.mk
 endif
 
-ifneq ($(BOARD_USES_QCOM_LEGACY),true)
+ifneq ($(BOARD_USES_LEGACY_QCOM),true)
 
 ifeq ($(TARGET_BOARD_PLATFORM),msm7x30)
-    include $(AUDIO_HW_ROOT)/msm7x30/Android.mk
+    include $(AUDIO_HW_ROOT)/7x30/Android.mk
 endif
 
 ifeq ($(TARGET_BOARD_PLATFORM),qsd8k)
