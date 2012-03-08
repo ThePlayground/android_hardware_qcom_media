6f09e4da58ef26e990305f7652014db613e9b976
TwistedUmbrella <twistedumbrella@gmail.com>
2012-03-08 00:25:38 -0500

    Deprecated modules based on CAF [Pre-CM KANG]

--- Android.mk
+++ Android.mk
@@ -1,12 +1,10 @@
 QCOM_MEDIA_ROOT := $(call my-dir)
 
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
+	include $(QCOM_MEDIA_ROOT)/libstagefrighthw/Android.mk
 
 ifneq ($(BOARD_USES_AUDIO_LEGACY),true)
 	include $(QCOM_MEDIA_ROOT)/audio/Android.mk
-endif
-ifneq ($(BOARD_USES_LEGACY_QCOM),true)
-	include $(QCOM_MEDIA_ROOT)/libstagefrighthw/Android.mk
 endif
 
 endif
--- audio/8x60/Android.mk
+++ audio/8x60/Android.mk
@@ -27,14 +27,14 @@ LOCAL_SHARED_LIBRARIES := \
     libacdbmapper
 
 ifneq ($(TARGET_SIMULATOR),true)
-LOCAL_SHARED_LIBRARIES += libdl
+    LOCAL_SHARED_LIBRARIES += libdl
 endif
 
 LOCAL_STATIC_LIBRARIES := \
     libmedia_helper \
     libaudiohw_legacy
 
-LOCAL_MODULE := audio.primary.$(TARGET_BOOTLOADER_BOARD_NAME)
+LOCAL_MODULE := audio.primary.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_MODULE_TAGS := optional
 
@@ -67,7 +67,7 @@ LOCAL_STATIC_LIBRARIES := \
     libmedia_helper \
     libaudiopolicy_legacy
 
-LOCAL_MODULE := audio_policy.$(TARGET_BOOTLOADER_BOARD_NAME)
+LOCAL_MODULE := audio_policy.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_MODULE_TAGS := optional
 
