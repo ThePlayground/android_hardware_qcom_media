c3715c8414bfdc7fd1d74a5880e5845f4397340d
TwistedUmbrella <twistedumbrella@gmail.com>
2012-03-13 01:14:25 -0400

    Remove duplicate libstagefrighthw but leave headers

--- Android.mk
+++ Android.mk
@@ -1,10 +1,7 @@
 QCOM_MEDIA_ROOT := $(call my-dir)
 
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
-ifneq ($(BOARD_USES_LEGACY_QCOM),true)
-	include $(QCOM_MEDIA_ROOT)/libstagefrighthw/Android.mk
-endif
-ifneq ($(BOARD_USES_AUDIO_LEGACY),true)
+    ifneq ($(BOARD_USES_AUDIO_LEGACY),true)
 	include $(QCOM_MEDIA_ROOT)/audio/Android.mk
-endif
+    endif
 endif
