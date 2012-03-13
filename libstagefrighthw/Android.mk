b026b1556e9007b12a497a1ba17765c7a86868be
TwistedUmbrella <twistedumbrella@gmail.com>
2012-03-13 01:08:07 -0400

    Cleanup libstagfrighthw build syntax to remove stray end

--- libstagefrighthw/Android.mk
+++ libstagefrighthw/Android.mk
@@ -1,8 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES := \
-    QComOMXPlugin.cpp                      \
+LOCAL_SRC_FILES := QComOMXPlugin.cpp
 
 LOCAL_CFLAGS := $(PV_CFLAGS_MINUS_VISIBILITY)
 
